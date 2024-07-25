82afec53f8986d63ae624f2bd0001314462a4ce1
Jerry Xiong <33706493+jxiong21029@users.noreply.github.com>
2024-07-25 18:29:12 -0400

    Add support for temporary instances (#777)
    * Add support for tmp instances
    
    * Change MINERL_TMP_INSTANCES flag
    
    Co-authored-by: Anssi <kaneran21@hotmail.com>
    
    ---------
    
    Co-authored-by: Anssi <kaneran21@hotmail.com>

--- minerl/env/malmo.py
+++ minerl/env/malmo.py
@@ -350,6 +350,7 @@ class MinecraftInstance(object):
         self.existing = existing
         self.minecraft_dir = None
         self.instance_dir = None
+        self.instance_tempdir = None
         self._status_dir = status_dir
         self.owner = None
         self._max_mem = max_mem
@@ -380,17 +381,40 @@ class MinecraftInstance(object):
             if not port:
                 port = InstanceManager._get_valid_port()
 
-            # self.instance_dir = tempfile.mkdtemp()
-            # self.minecraft_dir = os.path.join(self.instance_dir, 'Minecraft')
-            # shutil.copytree(os.path.join(InstanceManager.MINECRAFT_DIR), self.minecraft_dir,
-            #                 ignore=shutil.ignore_patterns('cache.properties.lock'))
-            # shutil.copytree(os.path.join(InstanceManager.SCHEMAS_DIR), os.path.join(self.instance_dir, 'Malmo', 'Schemas'))
-            self.minecraft_dir = InstanceManager.MINECRAFT_DIR
-            self.instance_dir = os.path.join(InstanceManager.MINECRAFT_DIR, '..')
+            if os.environ.get("MINERL_TMP_INSTANCES", "false").lower() in ("true", "1"):
+                # Copies built .jar and makes logs + save files write to a temporary
+                # directory to avoid issues e.g. when running on slurm.
+                self.instance_tempdir = tempfile.TemporaryDirectory()
+                self.instance_dir = self.instance_tempdir.name
+
+                self.minecraft_dir = os.path.join(self.instance_dir, "MCP-Reborn")
+                os.makedirs(os.path.join(self.minecraft_dir, "build"), exist_ok=True)
+                shutil.copytree(
+                    os.path.join(InstanceManager.MINECRAFT_DIR, "build", "libs"),
+                    os.path.join(self.minecraft_dir, "build", "libs"),
+                    ignore=shutil.ignore_patterns("cache.properties.lock")
+                )
+                shutil.copy2(
+                    os.path.join(InstanceManager.MINECRAFT_DIR, "launchClient.sh"),
+                    os.path.join(self.minecraft_dir, "launchClient.sh")
+                )
+                shutil.copy2(
+                    os.path.join(InstanceManager.MINECRAFT_DIR, "launchClient.bat"),
+                    os.path.join(self.minecraft_dir, "launchClient.bat")
+                )
+                shutil.copytree(
+                    os.path.join(InstanceManager.SCHEMAS_DIR),
+                    os.path.join(self.instance_dir, "Malmo", "Schemas"),
+                )
+            else:
+                # By default, runs with existing jar and puts logs + save files in
+                # minerl/MCP-Reborn.
+                self.minecraft_dir = InstanceManager.MINECRAFT_DIR
+                self.instance_dir = os.path.join(InstanceManager.MINECRAFT_DIR, '..')
 
             # 0. Get PID of launcher.
             parent_pid = os.getpid()
-            # 1. Launch minecraft process and 
+            # 1. Launch minecraft process
             self.minecraft_process = self._launch_minecraft(
                 port,
                 InstanceManager.headless,
@@ -647,7 +671,9 @@ class MinecraftInstance(object):
             if self in InstanceManager._instance_pool:
                 InstanceManager._instance_pool.remove(self)
                 self.release_lock()
-        pass
+
+            if self.instance_tempdir is not None:
+                self.instance_tempdir.cleanup()
 
     def __repr__(self):
         return ("Malmo[{}:{}, proc={}, addr={}:{}, locked={}]".format(
